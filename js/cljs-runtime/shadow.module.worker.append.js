14356116affd2b082c8af5023ef9bea7d38f2adf
Kevin Krausse (k0k01k9) <kevin.krausse@walmart.com>
2020-04-22 09:26:08 -0700

    rebuilt

--- js/cljs-runtime/shadow.module.worker.append.js
+++
@@ -1,6 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('worker');
-
-chess.worker.init();;
-SHADOW_ENV.setLoaded("chess.worker.js");
-SHADOW_ENV.setLoaded("shadow.module.worker.append.js");
