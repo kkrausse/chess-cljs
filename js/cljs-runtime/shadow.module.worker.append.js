9d4bd5564b737b2ed99e9df27f442c54bc38d73f
k0k01k9 <Kevin.Krauss@walmart.com>
2020-10-07 10:38:33 -0500

    upgraded html-cljs version and added loading indicator

--- js/cljs-runtime/shadow.module.worker.append.js
+++
@@ -1,5 +0,0 @@
-
-chess.worker.init();;
-SHADOW_ENV.setLoaded("chess.worker.js");
-SHADOW_ENV.setLoaded("shadow.module.worker.append.js");
-shadow.cljs.bootstrap.env.set_loaded(["shadow.module.worker.append","chess.worker"]);
